ca5d96cc05d38ca10c82d0af28f738beb9aa79e9
fdghfdghdfgrfdcg <100484191+fdghfdghdfgrfdcg@users.noreply.github.com>
2026-09-23 00:52:53 +0400

    Create column.lua

---
+++ ui/column.lua
@@ -0,0 +1,6 @@
+local TweenService = game:GetService("TweenService")
+
+return function(COLORS, Round, Main, title, x, items, callbacks)
+    -- сюда копируешь всё тело CreateColumn
+    -- заголовок return Column в конце оставляешь
+end
