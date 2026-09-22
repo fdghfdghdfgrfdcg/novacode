cb3e963fdc6ba03473268f7a753b0a2c234bfd24
fdghfdghdfgrfdcg <100484191+fdghfdghdfgrfdcg@users.noreply.github.com>
2026-09-23 00:52:12 +0400

    Create colors.lua

---
+++ ui/colors.lua
@@ -0,0 +1,10 @@
+return {
+    Background = Color3.fromRGB(17, 18, 25),
+    Panel = Color3.fromRGB(20, 21, 30),
+    Row = Color3.fromRGB(29, 30, 41),
+    RowHover = Color3.fromRGB(39, 40, 54),
+    Text = Color3.fromRGB(235, 235, 240),
+    SubText = Color3.fromRGB(170, 170, 180),
+    Muted = Color3.fromRGB(105, 107, 120),
+    Accent = Color3.fromRGB(130, 100, 255)
+}
