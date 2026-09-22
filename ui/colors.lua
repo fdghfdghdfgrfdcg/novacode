ac0b3044fb1867b58549507344317f1bb52601eb
fdghfdghdfgrfdcg <100484191+fdghfdghdfgrfdcg@users.noreply.github.com>
2026-09-23 01:22:23 +0400

    Update colors.lua

--- ui/colors.lua
+++ ui/colors.lua
@@ -3,8 +3,10 @@ return {
     Panel = Color3.fromRGB(20, 21, 30),
     Row = Color3.fromRGB(29, 30, 41),
     RowHover = Color3.fromRGB(39, 40, 54),
+
     Text = Color3.fromRGB(235, 235, 240),
     SubText = Color3.fromRGB(170, 170, 180),
     Muted = Color3.fromRGB(105, 107, 120),
+
     Accent = Color3.fromRGB(130, 100, 255)
 }
